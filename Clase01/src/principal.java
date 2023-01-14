13d684782a119a5fa78406033395323da1356214
Octavio Robleto <octavio.robleto@gmail.com>
2023-01-14 11:26:52 -0300

    Mi primer hola mundo

---
+++ Clase01/src/principal.java
@@ -0,0 +1,12 @@
+
+public class principal {
+
+	//este es el metodo principal de un programa en java y es lo primero que inicia
+	public static void main(String[] args) {
+		
+		//esto es un comando para imprimir en la consola
+		System.out.println("Hola Mundo");
+		
+		
+	}
+}
